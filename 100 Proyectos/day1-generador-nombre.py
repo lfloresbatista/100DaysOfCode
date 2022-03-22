b764231ee9c0f863d496b1e9dac56e410b25143f
Luis Flores <97482694+lfloresbatista@users.noreply.github.com>
2022-03-22 13:12:05 -0500

    Update day1-generador-nombre.py

--- 100 Proyectos/day1-generador-nombre.py
+++ 100 Proyectos/day1-generador-nombre.py
@@ -1,10 +1,4 @@
-#1. Create a greeting for your program.
 print("Hola bienvenidos al generador de nombres by Luis, este generador te puede funcionar ya sea para crear el nombre de una banda o crear el nombre de un emprendimiento\n\nVamos a Comenzar!!")
-#2. Ask the user for the city that they grew up in.
 ciudad = input("Cuál es el nombre de la ciudad donde naciste?\n")
-#3. Ask the user for the name of a pet.
 nombre_mascota = input("Cual es el nombre de tu mascota?\n")
-#4. Combine the name of their city and pet and show them their band name.
 print("El Nuevo nombre generado es: \n" + nombre_mascota + " de " + ciudad)
-#5. Make sure the input cursor shows on a new line, see the example at:
-#   https://replit.com/@appbrewery/band-name-generator-end
