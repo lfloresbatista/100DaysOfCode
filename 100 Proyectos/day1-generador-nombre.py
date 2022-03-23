d0622fd7fe26a97245de74aa064c87d61b1bb482
Luis Flores <97482694+lfloresbatista@users.noreply.github.com>
2022-03-22 19:06:37 -0500

    Update day1-generador-nombre.py
    Se creo y agrego la variable nombre_generado

--- 100 Proyectos/day1-generador-nombre.py
+++ 100 Proyectos/day1-generador-nombre.py
@@ -1,4 +1,5 @@
 print("Hola bienvenidos al generador de nombres by Luis, este generador te puede funcionar ya sea para crear el nombre de una banda o crear el nombre de un emprendimiento\n\nVamos a Comenzar!!")
 ciudad = input("Cuál es el nombre de la ciudad donde naciste?\n")
 nombre_mascota = input("Cual es el nombre de tu mascota?\n")
-print("El Nuevo nombre generado es: \n" + nombre_mascota + " de " + ciudad)
+nombre_generado = ("El Nuevo nombre generado es: \n" + nombre_mascota + " de " + ciudad)
+print(nombre_generado)
