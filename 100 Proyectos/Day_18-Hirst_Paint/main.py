f4c900d1aa832d3717a1909cb622a6e4857b3a72
Luis Flores <97482694+lfloresbatista@users.noreply.github.com>
2023-05-26 16:55:40 -0500

    Add files via upload

--- 100 Proyectos/Day_18-Hirst_Paint/main.py
+++ 100 Proyectos/Day_18-Hirst_Paint/main.py
@@ -1,1 +1,26 @@
+from turtle import Turtle, Screen
+from data import color_list
+import random
 
+hp = Turtle()
+screen = Screen()
+screen.colormode(255)
+# hp.pencolor(random.choice(color_list))
+x = -300
+y = -200
+hp.speed("fastest")
+hp.hideturtle()
+hp.up()
+hp.setpos(x, y)
+
+for _ in range(10):
+    for _ in range(10):
+        hp.dot(20, random.choice(color_list))
+        hp.fd(50)
+        y += 5
+    hp.setpos(x, y)
+
+screen.exitonclick()
+
+# TODO 10 x 10
+# todo size=20 space=50
